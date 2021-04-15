b4d1b4d2770ae6c1679cc4875fa14f80f4427ba1
Shyla Earl <shyla.earl@gmail.com>
2021-04-15 16:48:11 -0500

    updated html, created random number function, working on matching guesses to random number function

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,5 +1,27 @@
 $(document).ready(handleReady);
 
 function handleReady() {
-  console.log("jquery is loaded!")
-}
+  console.log("jquery is loaded!");
+  randomNumber(1, 25);
+  $('#submit-guesses').on('click', matchGuess);
+
+}//end handleReady
+
+function matchGuess(){
+  let player1Guess = Number($('#player-one').val());
+  let player2Guess = Number($('#player-two').val());
+  if(player1Guess === Math.floor(Math.random() * (1 + max - min) + min)){
+    return `Player 1, you guessed it!!`;
+  }//end player1 conditional
+  if(player2Guess === Math.floor(Math.random() * (1 + max - min) + min)){
+    return `Player 2, you guessed it!!`;
+  }//end player2 conditional
+  if(player1Guess !== Math.floor(Math.random() * (1 + max - min) + min))
+}//end matchGuess
+
+//add else if player is < or > math. blah return high/low
+
+function randomNumber(min, max){
+  return Math.floor(Math.random() * (1 + max - min) + min);
+}//end randomNumber
+
