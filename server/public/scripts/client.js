9db68fb5ce61ec9db81fcde9f7c9011f348fe5fa
Shyla Earl <shyla.earl@gmail.com>
2021-04-16 10:04:30 -0500

    wrote server and client POST route, newGuess input logging, click event function

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,26 +2,65 @@ $(document).ready(handleReady);
 
 function handleReady() {
   console.log("jquery is loaded!");
-  randomNumber(1, 25);
+  //randomNumber(1, 25);
   $('#submit-guesses').on('click', matchGuess);
-
+  
 }//end handleReady
 
+
 function matchGuess(){
-  let player1Guess = Number($('#player-one').val());
-  let player2Guess = Number($('#player-two').val());
-  if(player1Guess === Math.floor(Math.random() * (1 + max - min) + min)){
-    return `Player 1, you guessed it!!`;
-  }//end player1 conditional
-  if(player2Guess === Math.floor(Math.random() * (1 + max - min) + min)){
-    return `Player 2, you guessed it!!`;
-  }//end player2 conditional
-  if(player1Guess !== Math.floor(Math.random() * (1 + max - min) + min))
+  console.log('clicked', );
+  
+  let newGuesses = {
+    player1Guess: Number($('#player-one').val()),
+    player2Guess: Number($('#player-two').val()),
+  }
+  //send to server
+  console.log('Guesses:', newGuesses);
+
+  $.ajax({
+    method: 'POST',
+    url: '/guesses',
+    data: newGuesses,
+  })
+  .then(function (response){
+    console.log('Added guesses');
+    //changed data on server, get all updates
+    //getNewGuesses();
+  })
+  .catch(function(error){
+    console.log('Error from server', error);
+    alert('Something went wrong, try again later.');
+  })
+  
+  //clear inputs
+  $('#player-one').val('');
+  $('#player-two').val('');
+
 }//end matchGuess
 
-//add else if player is < or > math. blah return high/low
 
-function randomNumber(min, max){
-  return Math.floor(Math.random() * (1 + max - min) + min);
-}//end randomNumber
+
+// if(player1Guess === Math.floor(Math.random() * (1 + max - min) + min)){ 
+//   return alert(`Player 1, you guessed it!!`);
+// } else if (player1Guess > Math.floor(Math.random() * (1 + max - min) + min)){
+//   return alert(`Player 1, your guess is too high.`);
+// } else if (player1Guess < Math.floor(Math.random() * (1 + max - min) + min)){
+//   return `Player 1, your guess is too low.`;
+// }//end player1 conditional
+
+// if(player2Guess === Math.floor(Math.random() * (1 + max - min) + min)){
+//   return alert(`Player 2, you guessed it!!`);
+// } else if (player2Guess > Math.floor(Math.random() * (1 + max - min) + min)){
+//   return alert(`Player 2, your guess is too high.`);
+// } else if (player2Guess < Math.floor(Math.random() * (1 + max - min) + min)){
+//   return alert(`Player 2, your guess is too low.`);
+// } //end player2 conditional
+
+// function randomNumber(min, max){
+//    return Math.floor(Math.random() * (1 + max - min) + min);
+// }//end randomNumber
+
+//console.log('random number', randomNumber(1, 25));
+
 
--- server/server.js
+++ server/server.js
@@ -9,8 +9,27 @@ app.use(bodyParser.urlencoded({extended:true}))
 // Serve up static files (HTML, CSS, Client JS)
 app.use(express.static('server/public'));
 
+//TODO add array here to store data/history on the server
+  let guessHistory = [ ];
+
 // GET & POST Routes go here
 
+function randomNumber(min, max){
+  return Math.floor(Math.random() * (1 + max - min) + min);
+}//end randomNumber
+
+app.get('/guesses', (req, res) => {
+  res.send(newGuesses);
+})
+
+app.post('/guesses', (req, res) => {
+  let newGuesses = req.body;
+  console.log('New guesses', newGuesses);
+  //save new guesses
+  guessHistory.push(newGuesses);
+  res.sendStatus(201);
+  
+})
 
 app.listen(PORT, () => {
   console.log ('Server is running on port', PORT)
